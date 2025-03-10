72dc2e588984196bdcbe0e6d3be564c8919cbf87
ShrabanSah77 <shrabanshah77@gmail.com>
2025-03-10 06:52:20 -0500

    making pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/mainLayout.jsx";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<MainLayout />}>
+        <Route path= "/" element={<Login/>}/>
+        <Route path = "/" element={<Register/>}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
+    
 export default App;
---
+++ frontend/src/layouts/mainLayout.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <div className="Min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="bg-blue-600 text-white p-4 flex justify-between">
+        <h1 className="text-lg font-bold">Project1</h1>
+        <nav>
+          <Link to="/" className="px-3">
+            Home
+          </Link>
+          <Link to="/login" className="px-3">
+            Login
+          </Link>
+          <Link to="/register" className="px-3">
+            Register
+          </Link>
+        </nav>
+      </header>
+
+      {/* Main Content */}
+      <main className="flex-1 p-6">
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+
+      <footer className="bg-gray-800 text-white text-center p-3">
+        @ 2025 My App
+      </footer>
+    </div>
+  );
+};
+
+export default MainLayout;
