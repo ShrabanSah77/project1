82ee0b2ca5b92879ffd0359dc4cbf316cfa39824
ShrabanSah77 <shrabanshah77@gmail.com>
2025-03-10 07:55:39 -0500

    making login table

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Logging in with:", email, password);
+  };
+
+  return (
+    <div className="ma-w-wd mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-grey-700">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 border rounded-md"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-grey-700">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 border rounded-md"
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-md"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Login;
